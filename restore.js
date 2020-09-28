ae62773e00f286bd9b8f58a7da57713bf5ea605d
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-09-28 10:14:34 +0100

    scripts updated

--- restore.js
+++ restore.js
@@ -2,11 +2,10 @@ const { DB_URL } = require('./src/config/constants');
 const restore = require('mongodb-restore');
 const path = require('path');
 
-console.log('Ready to restore', path.join(__dirname, './db-dump/covid'));
+console.log('Ready to restore');
 restore({
   uri: DB_URL,
   root: path.join(__dirname, './db-dump'),
-  logger: __dirname,
   callback(err) {
     if (err) {
       console.error({ err });
