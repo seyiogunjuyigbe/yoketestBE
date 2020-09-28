6653c95321ebb24226a8906cce697bca43afb67d
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-09-28 11:32:29 +0100

    irrelevant models removed

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,9 @@
 const Point = require('./point');
 const Request = require('./request');
 const Symptom = require('./symptom');
-const Tag = require('./tag');
 
 module.exports = {
   Point,
   Request,
   Symptom,
-  Tag,
 };
